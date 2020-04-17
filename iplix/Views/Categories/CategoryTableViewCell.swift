6b13bdb4abb0841a14b912567b1e10edf18c3543
Farhan Adji <farhanadji@live.com>
2020-04-17 17:59:37 +0700

    Create Category Table View Cell

---
+++ iplix/Views/Categories/CategoryTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryTableViewCell.swift
+//  iplix
+//
+//  Created by Farhan Adji on 17/04/20.
+//  Copyright © 2020 Farhan Adji. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
