a670088d7a4372240530a8e7f144a4243580753f
Farhan Adji <farhanadji@live.com>
2020-04-20 17:40:08 +0700

    Create model production compay

---
+++ iplix/Model/MovieDetail.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDetail.swift
+//  iplix
+//
+//  Created by Farhan Adji on 20/04/20.
+//  Copyright © 2020 Farhan Adji. All rights reserved.
+//
+
+import Foundation
+
+struct MovieDetail: Decodable {
+    let production_companies: [ProductionCompany]
+}
---
+++ iplix/Model/ProductionCompany.swift
@@ -0,0 +1,14 @@
+//
+//  ProductionCompany.swift
+//  iplix
+//
+//  Created by Farhan Adji on 20/04/20.
+//  Copyright © 2020 Farhan Adji. All rights reserved.
+//
+
+import Foundation
+
+struct ProductionCompany: Decodable {
+    let id: Int?
+    let name: String?
+}
